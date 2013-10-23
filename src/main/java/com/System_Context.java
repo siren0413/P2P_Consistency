8cdb1da732850327bb1fbbb841b2b1a1b12a8930
Helen <helenwang789@gmail.com>
2013-10-23 13:34:20 -0500

    1099

--- src/main/java/com/System_Context.java
+++ src/main/java/com/System_Context.java
@@ -5,7 +5,7 @@ import java.util.concurrent.BlockingQueue;
 
 public class System_Context {
 
-	public static int SERVICE_PORT = 2055;
+	public static int SERVICE_PORT = 1099;
 	public static int BAND_WIDTH = 1;
 	public static BlockingQueue<String> downloadingQueue = new ArrayBlockingQueue<String>(100);
 	
